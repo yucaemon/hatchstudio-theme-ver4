c65359ea68bf89bfcd60cf1245be52d5ede37f42
Yuka Suzuki <suzukiyuca@gmail.com>
2018-12-13 17:11:23 -0800

    making 404 page

--- hatch-ver3/404.php
+++ hatch-ver3/404.php
@@ -1,35 +1,38 @@
 <?php /** *  Template Name:  404ページ */ ?>
 <?php include('head.php'); ?>
 <body>
-<section class='pages error-page'>
-<div class='outer'>
+<div class='error-page pages outer'>
 <?php include('components-php/header02.php'); ?>
 <?php include('components-php/header-sp.php'); ?>
-<div class='notice-bar'></div>
 <div class='outer__inner'>
 <div class='pages__container'>
-<div class='notfind__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/notfind.svg">
+<div class='notfind__header'>
+<div class='notfind__header__img'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/404-txt.svg">
 </div>
-<p class='notfind__text'>
+<p class='notfind__header__txt'>
 お探しのページが見つかりませんでした。
 </p>
+</div>
+<div class='notfind__body__img'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/404-img.svg">
+</div>
 <div class='notfind__btn'>
 <a href="<?php echo esc_url( home_url( '/' ) ); ?>">トップページへ戻る</a>
 </div>
 </div>
-<?php include('components-php/news-articles.php'); ?>
-<?php include('components-php/more-search-article.php'); ?>
-<div class='column-6 margin-center'>
-<?php include('components-php/sns-box.php'); ?>
 </div>
-<div class='column-8 margin-center'>
-<?php include('components-php/shopping-box.php'); ?>
-</div>
-<?php include('chatbox.php'); ?>
+<div class='outer__inner'>
+<div class='articles__container__bottom flexbox--h-center'>
+<?php include('components-php/bt-news-list.php'); ?>
+<?php include('components-php/bt-category-popular.php'); ?>
 </div>
 </div>
-</section>
+</div>
 </body>
+<div class='bottom-container'>
+<?php include('components-php/service.php'); ?>
+<?php include('components-php/annie-banner.php'); ?>
 <?php include('components-php/footer.php'); ?>
 <?php include('components-php/footer-sp.php'); ?>
+</div>
