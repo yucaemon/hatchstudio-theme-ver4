2b6185fd67a53887e1da759ef9485a6cf4a1bdae
Yuka Suzuki <suzukiyuca@gmail.com>
2019-01-05 17:21:15 -0800

    関連記事

--- hatch-ver3/components-php/lists-popular-month.php
+++ hatch-ver3/components-php/lists-popular-month.php
@@ -16,40 +16,41 @@ POPULOR
 </div>
 </div>
 </dt>
-<div class='news-list__container'></div>
-  <?php
-  $last_month = date('Y-m-d', strtotime('-10 months'));
-  $results = $wpdb->get_results("
-    SELECT posts.ID, meta.meta_value FROM $wpdb->postmeta as meta
-    join $wpdb->posts as posts on posts.ID = meta.post_id
-    WHERE meta.meta_key = 'views'
-    AND posts.post_status = 'publish'
-    AND posts.post_type = 'post'
-    AND posts.post_date > '$last_month';
-    ", ARRAY_N);
+<div class='news-list__container'>
+<?php
+$last_month = date('Y-m-d', strtotime('-10 months'));
+$results = $wpdb->get_results("
+  SELECT posts.ID, meta.meta_value FROM $wpdb->postmeta as meta
+  join $wpdb->posts as posts on posts.ID = meta.post_id
+  WHERE meta.meta_key = 'views'
+  AND posts.post_status = 'publish'
+  AND posts.post_type = 'post'
+  AND posts.post_date > '$last_month';
+  ", ARRAY_N);
 
-  $average_views = array();
-  foreach ($results as $result) {
-    $average_views[$result[0]] = $result[1];
-  }
+$average_views = array();
+foreach ($results as $result) {
+  $average_views[$result[0]] = $result[1];
+}
 
-  arsort( $average_views );
-  $view_ordered_post_ids = array_keys( $average_views )
-  ?>
+arsort( $average_views );
+$view_ordered_post_ids = array_keys( $average_views )
+?>
 
-  <?php foreach ( array_slice( $view_ordered_post_ids, 0, 5 ) as $this_post_index => $this_post_id ) {
-    $this_post_views = $average_views[$this_post_id];
-    $this_post = get_post( $this_post_id );
-  ?>
+<?php foreach ( array_slice( $view_ordered_post_ids, 0, 5 ) as $this_post_index => $this_post_id ) {
+  $this_post_views = $average_views[$this_post_id];
+  $this_post = get_post( $this_post_id );
+?>
 
-    <dd class="news-list__content">
-      <div class="iv class="news-list__thumb-img"">
-        <a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo get_the_post_thumbnail( $this_post->ID, 'thumbnail' ); ?></a>
-      </div>
-      <div class="news-list__header">
-        <p class="news-list__title><a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo $this_post->post_title; ?></a></p>
-      </div>
-    </dd>
+  <dd class="news-list__content flexbox">
+    <div class="news-list__thumb-img">
+      <a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo get_the_post_thumbnail( $this_post->ID, 'thumbnail' ); ?></a>
+    </div>
+    <div class="news-list__header">
+      <p class="news-list__title"><a href="<?php echo get_permalink($this_post->ID); ?>"><?php echo $this_post->post_title; ?></a></p>
+    </div>
+  </dd>
 
-  <?php } ?>
+<?php } ?>
+</div>
 </dl>
