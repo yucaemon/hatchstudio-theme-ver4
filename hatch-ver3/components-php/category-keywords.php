d9cba72ec1a32ad6c255593b54407cf0ca172c82
Yuka Suzuki <suzukiyuca@gmail.com>
2019-01-25 02:23:00 -0800

    modification

--- hatch-ver3/components-php/category-keywords.php
+++ hatch-ver3/components-php/category-keywords.php
@@ -2,46 +2,46 @@
 <div class='this-page__title components-title'>
 カテゴリー/エリアで探す
 </div>
-<div class='this-page__tags keywords'>
-<span class='tag'>
+<div class='this-page__tags keywords flexbox'>
+<div class='tag'>
 <a>
 サンフランシスコ
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 バークレー
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 UCバークレー大学
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 ポートランド
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 ニューヨーク
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 ハワイ
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 お知らせ/募集
 </a>
-</span>
-<span class='tag'>
+</div>
+<div class='tag'>
 <a>
 シェアハウス
 </a>
-</span>
+</div>
 </div>
 </div>
