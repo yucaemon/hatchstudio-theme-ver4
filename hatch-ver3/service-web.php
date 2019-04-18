07649dd430000297642c39225134b9a3c27747fa
Yuka Suzuki <suzukiyuca@gmail.com>
2019-04-18 16:17:07 -0700

    ・モバイル：サービスページ：WEB動画：bottmの修正

--- hatch-ver3/service-web.php
+++ hatch-ver3/service-web.php
@@ -44,9 +44,12 @@ WEB/動画制作
 <?php include('components-php/contact-form.php'); ?>
 </div>
 </body>
+<p id="PageTopBtn"><a href="#wrap">TOPへ</a></p>
 <div class='bottom-container'>
-<?php include('components-php/service.php'); ?>
 <?php include('components-php/annie-banner.php'); ?>
+<?php include('components-php/service.php'); ?>
+<?php include('components-php/business-sp.php'); ?>
+<?php include('components-php/annie-sp.php'); ?>
 <?php include('components-php/footer.php'); ?>
 <?php include('components-php/footer-sp.php'); ?>
 </div>
