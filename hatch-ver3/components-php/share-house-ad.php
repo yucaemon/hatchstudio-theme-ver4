95cde71515801a0d806159c6c2b1a44c790b16c5
Yuka Suzuki <suzukiyuca@gmail.com>
2019-05-11 17:50:20 -0700

    ・タブレット対応>index>はっちングハウスAD修正

--- hatch-ver3/components-php/share-house-ad.php
+++ hatch-ver3/components-php/share-house-ad.php
@@ -1,4 +1,5 @@
 <section class='section ad-banner hatching-house zigzag zigzag-blue'>
+<div class='flexbox'>
 <div class='hatching-house__header'>
 <div class='hatching-house__header--jp'>
 初渡米！女性一人旅でも安心なハウス
@@ -12,8 +13,12 @@
 </div>
 </div>
 </div>
-<div class='hatching-house__container flexbox'>
-<ul class='hatching-house__pictures flexbox'>
+<div class='hatching-house__btn--img'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/pink-guy.svg">
+</div>
+</div>
+<div class='hatching-house__container'>
+<ul class='hatching-house__pictures flexbox--spacebetween'>
 <li class='hatching-house--berkeley'>
 <a href="<?php echo esc_url( home_url( '/real-estate' ) ); ?>" class="megamenu-inner-text">
   <figure class="hatching-house--img">
@@ -60,8 +65,5 @@
 </div>
 </li>
 </ul>
-<div class='hatching-house__btn--img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/pink-guy.svg">
-</div>
 </div>
 </section>
