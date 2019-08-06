79948e8319ca5e672ad54bac999666bc930fc4b3
Yuka Suzuki <suzukiyuca@gmail.com>
2019-08-05 19:32:06 -0700

    ゲストハウスページの修正

--- hatch-ver3/hatching-house-pdx.php
+++ hatch-ver3/hatching-house-pdx.php
@@ -69,155 +69,263 @@ HATCHING HOUSE
 </div>
 </div>
 <div class='house-page__body__container'>
-<h2 class='house-page__body__header'>
-宿泊費用とアメニティ・設備
-</h2>
-<div class='house-page__body__price flexbox--spacebetween'>
-<div class='house-page__body__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/hatching-house-pdx-09.jpg">
-</div>
-<div class='house-page__body__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/hatching-house-pdx-08.jpg">
-</div>
-<div class='house-page__body__img'>
-<img src="<?php echo get_template_directory_uri(); ?>/images/hatching-house-pdx-07.jpg">
-</div>
-</div>
 <div class='house-page__body__pics flexbox--spacebetween'>
-<div class='house-page__body__img'>
+<div class='house-page__body__img price-pic'>
 <img src="<?php echo get_template_directory_uri(); ?>/images/room-c.jpg">
 </div>
-<div class='house-page__body__img'>
+<div class='house-page__body__img price-pic'>
 <img src="<?php echo get_template_directory_uri(); ?>/images/room-b.jpg">
 </div>
-<div class='house-page__body__img'>
+<div class='house-page__body__img price-pic'>
 <img src="<?php echo get_template_directory_uri(); ?>/images/room-a.jpg">
 </div>
 </div>
 <div class='house-page__body__read'>
 <div class='flexbox--spacebetween'>
-<ul class='house-page__body__detail'>
-<li>
-６ヶ月以上滞在の場合 長期割適用 $450/１ヶ月
+<div class='house-page__body__box'>
+<ul class='detail__lists lists__plice'>
+<li class='lists__plice--title'>
+お友達との節約旅行にぴったり♡
+<br>
+1F/cozy（コージー）/ROOM
 </li>
-<li>
-2名様滞在の場合 1名様あたり $15/1泊
-</li>
-<li>
-<i class="fas fa-bed"></i>
-キングサイズベット&シングルサイズベット
-</li>
-<li>
-タンス＆クローゼット
-</li>
-<li>
-<i class="fas fa-tint"></i>
-洗濯機、乾燥機
-</li>
-<li>
-<i class="fas fa-mug-hot"></i>
-タオル、ドライヤー
-</li>
-<li>
-<i class="fas fa-smoking-ban"></i>
-禁煙(外の庭でのみ喫煙可能)
-</li>
-<li>
-<i class="fas fa-utensils"></i>
-共同キッチン
-</li>
-<li>
-<i class="fas fa-car"></i>
-オプション 送迎あり
-</li>
-<li>
-暖房&ファン
+<li class='lists__plice--day'>
+$26/1泊
 </li>
 </ul>
-<ul class='house-page__body__detail'>
-<li>
-６ヶ月以上滞在の場合 長期割適用 $650/１ヶ月
-</li>
-<li>
-1名様のみ
-</li>
+<ul class='detail__lists lists__facility'>
 <li>
 <i class="fas fa-bed"></i>
-キングサイズベット&シングルサイズベット
+シングルサイズベット&クイーンソファーベット
 </li>
 <li>
-タンス＆クローゼット
+<i class="fas fa-bath"></i>
+共有シャワー、トイレ
 </li>
 <li>
-<i class="fas fa-tint"></i>
-洗濯機、乾燥機
+<i class="fas fa-user-plus"></i>
+追加料金$8で2名様まで宿泊可
 </li>
 <li>
-<i class="fas fa-mug-hot"></i>
-タオル、ドライヤー
-</li>
-<li>
-<i class="fas fa-smoking-ban"></i>
-禁煙(外の庭でのみ喫煙可能)
-</li>
-<li>
-<i class="fas fa-utensils"></i>
-共同キッチン
-</li>
-<li>
-<i class="fas fa-car"></i>
-オプション 送迎あり
+<i class="fas fa-award"></i>
+長期割引あり(4ヵ月以上滞在 最大50%引)
 </li>
 </ul>
-<ul class='house-page__body__detail'>
-<li>
-６ヶ月以上滞在の場合 長期割適用 $800/１ヶ月
+</div>
+<div class='house-page__body__box'>
+<ul class='detail__lists lists__plice'>
+<li class='lists__plice--title'>
+静かにゆったり♡女ひとり旅向け
+<br>
+2F/single (シングル)/ROOM
 </li>
-<li>
-2名様滞在の場合 1名様あたり $15/1泊
+<li class='lists__plice--day'>
+$34/1泊
 </li>
+</ul>
+<ul class='detail__lists lists__facility'>
 <li>
 <i class="fas fa-bed"></i>
 シングルサイズベット
 </li>
 <li>
-タンス＆クローゼット
+<i class="fas fa-bath"></i>
+共有シャワー、トイレ
+</li>
+<li>
+<i class="fas fa-user-plus"></i>
+おひとりさまのみ
+</li>
+<li>
+<i class="fas fa-award"></i>
+長期割引あり(4ヵ月以上滞在 最大50%引)
+</li>
+</ul>
+</div>
+<div class='house-page__body__box'>
+<ul class='detail__lists lists__plice'>
+<li class='lists__plice--title'>
+プライベートに過ごしたい♡カップル向き
+<br>
+2F/queen（クィーン）/ROOM
+</li>
+<li class='lists__plice--day'>
+$58/1泊
+</li>
+</ul>
+<ul class='detail__lists lists__facility'>
+<li>
+<i class="fas fa-bed"></i>
+ダブルサイズベット
+</li>
+<li>
+<i class="fas fa-bath"></i>
+プライベートトイレ＆バスルーム
+</li>
+<li>
+<i class="fas fa-user-plus"></i>
+プラス$8で2名様まで宿泊可
+</li>
+<li>
+<i class="fas fa-award"></i>
+長期割引あり(4ヵ月以上滞在 最大50%引)
+</li>
+</ul>
+</div>
+<div class='house-page__body__detail'>
+<h2 class='house-page__body__header'>
+アメニティ・設備
+</h2>
+<div class='flexbox--spacebetween'>
+<ul>
+<li>
+<i class="far fa-clock"></i>
+チェックイン: 1pm-10pm
+</li>
+<li>
+<i class="fas fa-bell"></i>
+チェックアウト: 11amまで
+</li>
+<li>
+<i class="fas fa-suitcase"></i>
+荷物のお預かりOK(事前予約要）
+</li>
+<li>
+<i class="fas fa-money-bill"></i>
+当日現金支払いのみ
+</li>
+<li>
+<i class="fas fa-ban"></i>
+当日キャンセル可
+</li>
+</ul>
+<ul>
+<li>
+<i class="fas fa-wifi"></i>
+WIFI（無料インターネット）
+</li>
+<li>
+<i class="fas fa-tshirt"></i>
+クローゼット（ハンガー付）
+</li>
+<li>
+<i class="fas fa-shower"></i>
+タオル、ドライヤー、トイレットペーパー
 </li>
 <li>
 <i class="fas fa-tint"></i>
-洗濯機、乾燥機
-</li>
-<li>
-<i class="fas fa-mug-hot"></i>
-タオル、ドライヤー
-</li>
-<li>
-<i class="fas fa-smoking-ban"></i>
-禁煙(外の庭でのみ喫煙可能)
+洗濯機、乾燥機あり(洗剤は各自で用意)
 </li>
 <li>
 <i class="fas fa-utensils"></i>
-共同キッチン
+共同キッチン（お皿など調理器具、冷蔵庫スペース）
+</li>
+</ul>
+<ul>
+<li>
+<i class="fas fa-smoking-ban"></i>
+禁煙(外庭でのみ喫煙可)
+</li>
+<li>
+<i class="fas fa-wind"></i>
+エアコン暖房
+</li>
+<li>
+<i class="fas fa-parking"></i>
+無料路上駐車場
 </li>
 <li>
 <i class="fas fa-car"></i>
-オプション 送迎あり
+オプション：有料送迎（事前予約要:1回$35）
 </li>
 </ul>
 </div>
 </div>
-<div class='house-page__body__notice'>
-<p>
-<i class="fas fa-exclamation-triangle"></i>
-注意書き
+</div>
+</div>
+<div class='house-page__body__notice room-notice'>
+<h2 class='house-page__body__header'>
+注意事項
+</h2>
+<p class='room-notice--title'>
+チェックアウト、チェックイン時間について
 </p>
-<p>
-※写真イメージは、個室（大）お部屋には、プライベート用のシャワー、トイレがつきますが、個室(A)(B)のお部屋は、シャワー、トイレは共同になります。
+<p class='room-notice--txt'>
+チェックアウト、チェックイン時間は、基本的に上記の時間帯でお願いしておりますが、予約状況によっては、事前（ご予約の日の2日前まで）にご相談いただいているお客様のみ、お客様の都合時間に合わせたチェックアウト、チェックイン時間帯に合わせることができる場合がございますので、ご相談ください。事前にご相談がない上記時間以外のチェックアウト、チェックインは、違反扱い/自動キャンセルの対象とさせていて頂くことがございますのでご了承ください。
 </p>
-<p>
-※個室(B)のお部屋は、一般的なお部屋とあまり変わりませんが、もともとお家のガレージの一部のようなお部屋を改造して作った為、格安のお部屋です。ご了承いただける方のみご予約の方お願いいたします。また、個室(B)のお部屋は、ベットは2つございますので、ご友人との宿泊も便利だと思います。お部屋の写真を見たい方は、お問い合わせください。
+<p class='room-notice--title'>
+荷物のお預かりについて
 </p>
-<p>
+<p class='room-notice--txt'>
+荷物のお預かりは、事前（ご予約の日の2日前まで）ご相談頂けてましたら、チェックイン時間以外でも荷物をお預かりすることができる場合がありますのでご相談ください。 （例：早朝の便でチェックイン開始時間までどこかで待つ必要があるが荷物だけは預かっていてもらいたい。等など)
+</p>
+<p class='room-notice--title'>
+お支払いについて
+</p>
+<p class='room-notice--txt'>
+お支払いは、お部屋に到着してから、基本的にアメリカドル現金でのみお願いしております。（他にVenmo,米銀行小切手、米ペイパルの支払い可能）
+</p>
+<p class='room-notice--title'>
+予約手続きについて
+</p>
+<P class='room-notice--txt'>
+予約手続きについては、ご予約したい日にち、チェックイン希望時間を記入の上、以下の問い合わせフォームにて、ご連絡ください。
+</P>
+<p class='room-notice--title'>
+予約キャンセルついて
+</p>
+<P class='room-notice--txt'>
+予約キャンセルついては、お支払いいただく前であれば、いつでもキャンセルできます。ただし、お支払い後のキャンセル、ご返金はできかねますのでご注意ください。また、ご予約いただいている日のチェックイン時間[夜11pm]までに、ご連絡が取れない場合は、ご予約頂いている日にちを全て自動キャンセル扱いとさせて頂いております。
+</P>
+<p class='room-notice--title'>
+共同トイレ、シャワーの利用について
+</p>
+<p class='room-notice--txt'>
+※cozy（コージー）,single(シングル)部屋は、共同トイレ、シャワーで入居者(男女)と共有になるため、時間帯によっては、混み合う場合がございますのでご了承ください。シャワー使用時間は、いつでも使用していただいて構いません。
+</p>
+<p class='room-notice--title'>
+洗濯機、乾燥機の使用時間について
+</p>
+<p class='room-notice--txt'>
+※洗濯機、乾燥機は室内にあり、無料で使えますが、使用時間は、朝７時ー夜11時の間のみさせて頂きます。洗剤とは各自で用意していただくことになります。
+</p>
+<p class='room-notice--title'>
+キッチンの利用について
+</p>
+<p class='room-notice--txt'>
+※キッチンは、いつでも使っていただいて構いません。キッチにおいてある調理器具（レンジ、フライパン、日本　製炊飯器、お鍋、お皿、コップ、箸、スプーン、フォーク、コーヒーメーカーetc）は自由に使えます。また、冷蔵庫もゲスト専用のスペースがありますのでお使いいただけます。
+</p>
+<p class='room-notice--title'>
+1F/cozy/ROOMの部屋について
+</p>
+<p class='room-notice--txt'>
+※cozy(コージー)ROOMのお部屋は、一般的なドア付きの個室のお部屋とあまり変わりませんが、お家の一部をお部屋を改造して作った為、格安のお部屋です。ご了承いただける方のみご予約の方お願いいたします。
+</p>
+<p class='room-notice--title'>
+送迎サービスについて
+</p>
+<p class='room-notice--txt'>
+※送迎を依頼する場合は、予めのご予約をお願いいたします。場合によっては、ご希望の日にちにお答えできかねる場合がありますので、早めのご予約をお願いいたします。送迎は、[ポートランド空港ーシェアハウス]１回$35になります。支払いは、お部屋代の時にまとめて、現金のみで頂いております。
+</p>
+<p class='room-notice--title'>
+長期割引の適用について
+</p>
+<p class='room-notice--txt'>
+※4ヶ月以上滞在の場合 長期割が適用できます。場合によっては、最大で50%割引ができますので、ご相談ください。また長期4ヶ月以上滞在の場合は、デポジット（１ヵ月分）を最初にいただく事になり、物破損、4ヶ月以内の退去などの違反がない限り、退去時に全額返金します。
+</p>
+<p class='room-notice--title'>
+WIFIインターネットの利用について
+</p>
+<p class='room-notice--txt'>
+※インターネットは、お部屋に着いたらお部屋に書いてある『パスワード』でログインして頂ければ、無線でご自由に使えます。スピードも問題ありません。
+</p>
+<p class='room-notice--title'>
+ポートランドの飲み水について
+</p>
+<p class='room-notice--txt'>
+※飲み水等は、基本的にお家の蛇口のお水を安心してお飲みになれます。ポートランドは、自然に囲まれていているだけあって、蛇口からでもそのままお水はとっても安全で美味しく飲めます。
+</p>
+<p class='room-notice--title'>
 ※お部屋のご予約、ご質問等などお気軽に以下のお問い合わせフォームにて、ご相談ください。
 </p>
 </div>
