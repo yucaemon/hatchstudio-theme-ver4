aa183c70f498fc825dd6e3ba566356181bff82c0
Yuka Suzuki <suzukiyuca@gmail.com>
2020-04-24 15:53:28 -0700

    おすすめ記事に広告を挿入

--- hatch-ver3/components-php/bt-recommend.php
+++ hatch-ver3/components-php/bt-recommend.php
@@ -14,8 +14,32 @@
   'category' => '788' // テスト環境おすすめID630-本番ID788
   ));
   ?>
+  <?php
+  $ads_infeed = array(1,5,9); //何番目に表示したいか
+  $ads_infeed_count = '1';
+  ?>
   <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
     <!--表示する内容が入ります。-->
+    <?php
+    $ads_disp = in_array($ads_infeed_count, $ads_infeed);
+    if($ads_disp){
+    ?>
+    <!-- ここにインフィード広告のコードを入れる -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-format="fluid"
+         data-ad-layout-key="-7l+f1-1g-4q+e1"
+         data-ad-client="ca-pub-1574488309106788"
+         data-ad-slot="3447327512"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <?php
+    }
+    $ads_infeed_count++;
+    ?>
+
     <li class='eyecatch'>
       <div class='post-lists__img article-list__img imgWrap'><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
       <div class="post-lists__text article-list__text"><p class="post-lists__title list-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p></div>
