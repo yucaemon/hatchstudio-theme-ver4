9f18cfb40417b4d2340733c3319138055433f62b
Yuka Suzuki <suzukiyuca@gmail.com>
2020-04-25 19:37:51 -0700

    modification

--- hatch-ver3/components-php/bt-recommend.php
+++ hatch-ver3/components-php/bt-recommend.php
@@ -26,16 +26,19 @@
     ?>
     <!-- ここにインフィード広告のコードを入れる -->
     <li class='eyecatch'>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      <ins class="adsbygoogle"
-           style="display:block; text-align:center;"
-           data-ad-layout="in-article"
-           data-ad-format="fluid"
-           data-ad-client="ca-pub-1574488309106788"
-           data-ad-slot="2119656172"></ins>
-      <script>
-           (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+      <?php if (is_single(array('25907','25713','7586'))) : ?>
+      <?php else: ?>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:block; text-align:center;"
+             data-ad-layout="in-article"
+             data-ad-format="fluid"
+             data-ad-client="ca-pub-1574488309106788"
+             data-ad-slot="2119656172"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      <?php endif; ?>
       <?php
       }
       $ads_infeed_count++;
