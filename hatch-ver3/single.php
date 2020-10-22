5b5d06786d14e21e877fbbf30a7d59d3207e9053
YUKA SUZUKI <yukasuzuki@YUKAnoMacBook-Air.local>
2020-10-21 19:32:54 -0700

    test7

--- hatch-ver3/single.php
+++ hatch-ver3/single.php
@@ -29,23 +29,12 @@
 <!-- H1タイトル下の広告 -->
 <?php if (is_single(array('25907','25713','7586'))) : ?>
 <?php else: ?>
-<!-- script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-1574488309106788"
      data-ad-slot="8294607318"
      data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script -->
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- ラージ スカイスクレイパー！(リンク検索広告) -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1574488309106788"
-     data-ad-slot="2908696842"
-     data-ad-format="link"
      data-full-width-responsive="true"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
