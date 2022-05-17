ee031236666f04d86d055b429e60aaef3d48f730
yuka <suzukiyuca@gmail.com>
2022-05-16 19:30:03 -0700

    カテゴリーページと一覧ページの修正

--- hatch-ver4/archive.php
+++ hatch-ver4/archive.php
@@ -1,45 +1,168 @@
 <?php /** *  Template Name:  一覧ページ */ ?>
 <?php include('head.php'); ?>
-<body></body>
-<section class='pages rules-page'>
-<div class='outer'>
-<?php include('components-php/header03.php'); ?>
-<div class='archive-banner'>
-<h1 class='archive-banner__header'>
-記事一覧ページ
-</h1>
+<body class='fuatured-page'>
+<div class='pages outer'>
+<?php include('components-php/header02.php'); ?>
+<?php include('components-php/header-sp.php'); ?>
+<div class='pages__container__title'>
+<div class='outer__inner'>
+<div class='title-bar flexbox--h-center'>
+<div class='title-bar-txt'>
+アメリカーンなものを食う
+</div>
+<div class='title-bar-img'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__bad-food.svg">
 </div>
 </div>
-<div class='outer__inner column-12'>
-<div class='articles news-articles'>
-<?php
-$posts = get_posts(array(
-'posts_per_page' => 50, // 表示件数
-));
-?>
-<div class="news-articles__container flexbox--spacebetween">
-  <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
-      <!--表示する内容が入ります。-->
-      <div class="news-articles__content">
-        <div class="news-articles__img">
-          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-        </div>
-        <div class="news-articles__text">
-          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </div>
-      </div>
-      <!--表示する内容ここまで-->
-  <?php endforeach; endif; ?>
 </div>
 </div>
-<?php include('components-php/more-search-article.php'); ?>
-<div class='column-6 margin-center'>
-<?php include('components-php/sns-box.php'); ?>
+<div class='pages__container__ranking zigzag zigzag-pink'>
+<div class='outer__inner ranking__boxs flexbox--h-center'>
+<div class='ranking__box'>
+<div class='ranking__box-icon'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking01.svg">
 </div>
-<div class='column-8 margin-center'>
-<?php include('components-php/shopping-box.php'); ?>
+<div class='ranking__box-pic'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
 </div>
-<?php include('chatbox.php'); ?>
+<div class='ranking__box-txt'>
+テキストテキストテキストテキストテキストテキストテキストテキスト
 </div>
+</div>
+<div class='ranking__box'>
+<div class='ranking__box-icon'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking02.svg">
+</div>
+<div class='ranking__box-pic'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img02.svg">
+</div>
+<div class='ranking__box-txt'>
+テキストテキストテキストテキストテキストテキストテキストテキスト
+</div>
+</div>
+<div class='ranking__box'>
+<div class='ranking__box-icon'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking03.svg">
+</div>
+<div class='ranking__box-pic'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img03.svg">
+</div>
+<div class='ranking__box-txt'>
+テキストテキストテキストテキストテキストテキストテキストテキスト
+</div>
+</div>
+</div>
+</div>
+<div class='pages__container__tag-related-article'>
+<div class='outer__inner tag-related-article'>
+<div class='tag-related-article-title'>
+『アメリカカ〜ンなモノを食う。』新着記事一覧
+</div>
+<div class='tag-related-article-body two-column-sp four-col-last-left'>
+<ul class='article-list post-lists flexbox--spacebetween'>
+  <?php
+     $args = array(
+    'posts_per_page' => 15,                //表示（取得）する記事の数
+    'post_type' => 'post'    //投稿タイプの指定
+  );
+  $posts = get_posts( $args );
+    if( $posts ) : foreach( $posts as $post) : setup_postdata( $post ); ?>
+
+        <li class='eyecatch'>
+          <?php
+            $days = 7;  // NEWマークを表示する日数
+            $now = date_i18n('U');  // 今の時間
+            $entry = get_the_time('U');  // 投稿日の時間
+            $term = date('U',($now - $entry)) / 86400;
+            if( $days > $term ){
+              echo '<div class="newMark eyechatchlabel">NEW</div>';
+            }
+          ?>
+
+
+          <div class='post-lists__img article-list__img imgWrap'>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array( 230, 230 ) ); ?></a>
+          </div>
+          <p class="eyechatchlabel <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>">
+            <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?>
+          </p>
+
+          <div class='post-lists__text article-list__text'>
+            <h3 class='post-lists__title list-title'>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+          </div>
+        </li>
+
+    <?php endforeach; ?>
+    <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+    <?php else : //記事が無い場合 ?>
+      <li><p>記事はまだありません。</p></li>
+    <?php endif;
+    wp_reset_postdata(); //クエリのリセット ?>
+</ul>
+<div class='btn-img--toallpage'>
+<a href="https://hatchstudioinc.com/archives/24020" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/toallpage-btn.svg"></a>
+</div>
+</div>
+</div>
+</div>
+<div class='pages__container__tag-pickup-article zigzag zigzag-yellow'>
+<div class='outer__inner tag-pickup-article__boxs flexbox--spacearound'>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+アメリカのスーパー
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+潜入
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+食い比べ
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+トレジョ
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+レシピ
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+珍もの＆まずいもの検証
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'>
+流行り
+</div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'></div>
+</div>
+<div class='tag-pickup-article__box'>
+<img src="<?php echo get_template_directory_uri(); ?>/images/featured-page__rinking-img01.svg">
+<div class='cat-label'></div>
+</div>
+</div>
+</div>
+<?php include('components-php/Q&A-banner.php'); ?>
 <?php include('components-php/footer.php'); ?>
-</section>
+<?php include('components-php/footer-sp.php'); ?>
+</div>
+</body>
--- hatch-ver4/category.php
+++ hatch-ver4/category.php
@@ -1,95 +1,55 @@
 <?php /** *  Template Name: カテゴリーページ */ ?>
 <?php include('head.php'); ?>
-<body>
-<div class='category-page category outer'>
+<?php include('head.php'); ?>
+<body class='category-page'>
+<div class='pages outer'>
 <?php include('components-php/header02.php'); ?>
 <?php include('components-php/header-sp.php'); ?>
-<div class='outer__inner'>
-<div class='category__container'>
-<div class='category__container__body flexbox--spacebetween'>
-<div class='middle-contain'>
-<h1 class='components-title'>
-『 <?php echo get_the_archive_title(); ?> 』の関連記事一覧
-</h1>
-<ul class='post-lists article-list box-list flexbox'>
-<?php
-$cat_id = get_cat_ID( get_the_archive_title() );
-if ( $cat_id ) {
-    $args = array(
-        'posts_per_page' => 270, // 表示件数の指定
-        'category' => $cat_id,
-    );
-}
-    $posts = get_posts( $args );
-    foreach ( $posts as $post ): // ループの開始
-setup_postdata( $post ); // 記事データの取得
-?>
-  <li class=''>
-    <div class='post-lists__img article-list__img'>
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array( 230, 230 ) ); ?></a>
-    </div>
-    <div class='post-lists__text article-list__text'>
-      <h3 class='post-lists__title list-title'>
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-      </h3>
-    </div>
-  </li>
-<?php
-endforeach; // ループの終了
-wp_reset_postdata(); // 直前のクエリを復元する
-?>
+『<?php echo get_the_archive_title(); ?> 』の関連記事一覧
+<div class='outer__inner category-article flexbox--h-center'>
+<div class='category-article-body two-column-sp four-col-last-left'></div>
+<ul class='article-list post-lists flexbox--spacebetween'>
+  <?php
+     $args = array(
+    'posts_per_page' => 15,                //表示（取得）する記事の数
+    'post_type' => 'post'    //投稿タイプの指定
+  );
+  $posts = get_posts( $args );
+    if( $posts ) : foreach( $posts as $post) : setup_postdata( $post ); ?>
+
+        <li class='eyecatch'>
+          <?php
+            $days = 7;  // NEWマークを表示する日数
+            $now = date_i18n('U');  // 今の時間
+            $entry = get_the_time('U');  // 投稿日の時間
+            $term = date('U',($now - $entry)) / 86400;
+            if( $days > $term ){
+              echo '<div class="newMark eyechatchlabel">NEW</div>';
+            }
+          ?>
+
+
+          <div class='post-lists__img article-list__img imgWrap'>
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array( 230, 230 ) ); ?></a>
+          </div>
+          <p class="eyechatchlabel <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->slug; } ?>">
+            <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?>
+          </p>
+
+          <div class='post-lists__text article-list__text'>
+            <h3 class='post-lists__title list-title'>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+          </div>
+        </li>
+
+    <?php endforeach; ?>
+    <?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+    <?php else : //記事が無い場合 ?>
+      <li><p>記事はまだありません。</p></li>
+    <?php endif;
+    wp_reset_postdata(); //クエリのリセット ?>
 </ul>
-<?php include('components-php/category-keywords.php'); ?>
-<?php include('components-php/affiliate-ad-sky.php'); ?>
-</div>
-<div class='side-contain'>
-<?php include('components-php/affiliate-ad-sky.php'); ?>
-<div class='side-ranking-ad'>
-<a href="https://hatchstudioinc.com/archives/24020" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/ranking-banner.svg"></a>
-</div>
-<div class='side-job-ad'>
-<a href="https://hatchstudioinc.com/archives/28793" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/remote-job-banner.svg"></a>
-</div>
-<div class='side-coupon-ad'>
-<a href="https://hatchstudioinc.com/archives/28854" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/coupon-banner.svg"></a>
-</div>
-<?php include('components-php/lists-news.php'); ?>
-<?php include('components-php/keywords.php'); ?>
-<!-- /<?php include('components-php/annie-ad-side.php'); ?> -->
-<?php include('components-php/house-ad-side.php'); ?>
-<?php include('components-php/business-ad-side.php'); ?>
-<div class='side-ranking-ad'>
-<a href="https://hatchstudioinc.com/archives/24020" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/ranking-banner.svg"></a>
-</div>
-<div class='side-job-ad'>
-<a href="https://hatchstudioinc.com/archives/28793" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/remote-job-banner.svg"></a>
-</div>
-<div class='side-coupon-ad'>
-<a href="https://hatchstudioinc.com/archives/28854" class=""><img src="<?php echo get_template_directory_uri(); ?>/images/coupon-banner.svg"></a>
-</div>
-<div class='side-fixed-area'></div>
-</div>
-</div>
-</div>
-</div>
-<div class='outer__inner'>
-<div class='articles__container__bottom flexbox--spacebetween'>
-<div class='bt-left'>
-<?php include('components-php/bt-news-lists.php'); ?>
-</div>
-<div class='bt-right'>
-<!-- ?php include('components-php/affiliate-ad-article.php'); ? -->
-<?php include('components-php/bt-popular-month.php'); ?>
-</div>
-</div>
-</div>
-<p id="PageTopBtn"><a href="#wrap">TOPへ</a></p>
-<div class='bottom-container'>
-<!-- /<?php include('components-php/service.php'); ?> -->
-<!-- /<?php include('components-php/business-sp.php'); ?> -->
-<!-- /<?php include('components-php/annie-banner.php'); ?> -->
-<?php include('components-php/footer.php'); ?>
-<?php include('components-php/footer-sp.php'); ?>
 </div>
 </div>
 </body>
