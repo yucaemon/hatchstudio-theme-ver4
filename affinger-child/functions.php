87e58ae5cc799355f8a9413bd3b17ad50a72ba43
yuka <suzukiyuca@gmail.com>
2023-02-10 18:01:59 -0800

    カテゴリー一覧ページ：ウェジットでPHPが使えるようにした。

--- affinger-child/functions.php
+++ affinger-child/functions.php
@@ -2,3 +2,15 @@
 if ( !defined( 'ABSPATH' ) ) {
 exit;
 }
+
+//ウィジェットでPHPを使えるようにする。
+function widget_text_exec_php( $widget_text ) {
+if( strpos( $widget_text, '<' . '?' ) !== false ) {
+ob_start();
+eval( '?>' . $widget_text );
+$widget_text = ob_get_contents();
+ob_end_clean();
+}
+return $widget_text;
+}
+add_filter( 'widget_text', 'widget_text_exec_php', 99 );
